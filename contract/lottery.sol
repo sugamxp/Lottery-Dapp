fb85781246d713d0a5c6a790d5c2528cb7c38cca
Sugam Devare <sugamxp@gmail.com>
2018-08-10 16:36:28 +0530

    working rinkeby

--- contract/lottery.sol
+++ contract/lottery.sol
@@ -2,34 +2,33 @@ pragma solidity ^0.4.20;
 
 contract Casino{
 
-   //Variables 
-   address public owner;
-   uint256 public minBet;
-   uint256 public numberOfBets;
+    address public owner;
+   uint256 public minimumBet;
    uint256 public totalBet;
+   uint256 public numberOfBets;
    uint256 public maxAmountOfBets = 100;
-   uint256 public lastWinner = -1;
+   uint256 public lastWinner = 0;
    address[] public players;
-	
-   struct Player {
+   
+
+      struct Player {
       uint256 amountBet;
       uint256 numberSelected;
    }
 
-   //Like a dictionary with the address being the key and Player struct being the value
    mapping(address => Player) public playerInfo;
 
-	constructor(uint256 _minBet) public{
-		owner = msg.sender;
-		if(_minBet != 0 ) minBet = _minBet;
+   constructor(uint256 _minimumBet) public{
+      owner = msg.sender;
+      if(_minimumBet != 0 ) minimumBet = _minimumBet;
 
-	}
+   }
 
-	function kill() public{
-		if(msg.sender == owner) selfdestruct(owner);
-	}
+   function kill() public{
+      if(msg.sender == owner) selfdestruct(owner);
+   }
 
-	 function checkPlayerExists(address player) public constant returns(bool){
+    function checkPlayerExists(address player) public constant returns(bool){
       for(uint256 i = 0; i < players.length; i++){
          if(players[i] == player) return true;
       }
@@ -37,41 +36,41 @@ contract Casino{
    }
 
 
-		function bet(uint256 numberSelected) public payable {
-		require(checkPlayerExists(msg.sender)==false);
-		require(numberSelected >= 1 && numberSelected <= 10);
-		require(msg.value >= minBet);
-		playerInfo[msg.sender].amountBet = msg.value;
-		playerInfo[msg.sender].numberSelected = numberSelected;
-		numberOfBets++;
-		players.push(msg.sender);
-		totalBet += msg.value;
-		if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
-	}
+      function bet(uint256 numberSelected) public payable {
+      require(checkPlayerExists(msg.sender)==false);
+      require(numberSelected >= 1 && numberSelected <= 10);
+      require(msg.value >= minimumBet);
+      playerInfo[msg.sender].amountBet = msg.value;
+      playerInfo[msg.sender].numberSelected = numberSelected;
+      numberOfBets++;
+      players.push(msg.sender);
+      totalBet += msg.value;
+      if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
+   }
 
-	function generateNumberWinner() public {
-      uint256 numberGenerated = uint256(block.blockhash(block.number-1))%10 + 1;
+   function generateNumberWinner() public {
+      uint256 numberGenerated = block.number % 10 + 1; // This isn't secure
       distributePrizes(numberGenerated);
    }
 
    function distributePrizes(uint256 numberWinner) public{
-   	address[100] memory winners;
-   	uint256 count = 0;
+      address[100] memory winners;
+      uint256 count = 0;
     lastWinner = numberWinner;
-   	for(uint256 i=0; i<players.length; i++){
-   		address playerAddress = players[i];
-   		if(playerInfo[playerAddress].numberSelected == numberWinner){
-   			winners[count] = playerAddress;
-   			count++;
-   	}
-   	delete playerInfo[playerAddress];
+      for(uint256 i=0; i<players.length; i++){
+         address playerAddress = players[i];
+         if(playerInfo[playerAddress].numberSelected == numberWinner){
+            winners[count] = playerAddress;
+            count++;
+      }
+      delete playerInfo[playerAddress];
    }
    players.length = 0;
 
    uint256 winnerEtherAmount = totalBet / winners.length;
    for(uint256 j=0;j<count;j++){
-   	 if(winners[j] != address(0))
-   	 	winners[j].transfer(winnerEtherAmount);
+       if(winners[j] != address(0))
+         winners[j].transfer(winnerEtherAmount);
    }
     
    }
--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,18 @@ var Web3 = require('web3');
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 } else {
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"))
+    //For offline blockchain
+    web3 = new Web3(new Web3.providers.HttpProvider("https://localhost:7545"));
 }
-
+console.log(web3.currentProvider);
 web3.eth.defaultAccount = web3.eth.accounts[0];
+console.log('hello');
 
-var LotContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"numberSelected","type":"uint256"}],"name":"bet","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"numberWinner","type":"uint256"}],"name":"distributePrizes","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"generateNumberWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_minimumBet","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"checkPlayerExists","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maxAmountOfBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"minimumBet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"numberOfBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"playerInfo","outputs":[{"name":"amountBet","type":"uint256"},{"name":"numberSelected","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalBet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]);
-var lot = LotContract.at('0x3b04b2d0ede9d3c0df23922c8f8d40a471c8ff61');
+console.log(web3.eth.defaultAccount)
+//ABI goes here
+var LotContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"numberSelected","type":"uint256"}],"name":"bet","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"numberWinner","type":"uint256"}],"name":"distributePrizes","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"generateNumberWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_minimumBet","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"checkPlayerExists","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lastWinner","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maxAmountOfBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"minimumBet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"numberOfBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"playerInfo","outputs":[{"name":"amountBet","type":"uint256"},{"name":"numberSelected","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalBet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]);
+//Contract address goes here
+var lot = LotContract.at('0xb462545e52d670e4a69bb497bb6e2edcf1347f0e');
 
 console.log(lot);
 
@@ -42,7 +47,7 @@ function displayInfo() {
         $('#txt1').text(result);
     });
 
-     lot.minBet.call(function(error, result) {
+     lot.minimumBet.call(function(error, result) {
         $('#txt4').text(result);
     });
 
